4e7fcef1e41313de95366d22107140bf255c9efb
sdcb <sdflysha@qq.com>
2024-05-28 12:11:11 +0800

    Add CDLobby

---
+++ SpinBladeArena/LogicCenter/Lobbies/ChickenDinnerLobby.cs
@@ -0,0 +1,9 @@
+﻿namespace SpinBladeArena.LogicCenter.Lobbies;
+
+public class ChickenDinnerLobby(int id, int createUserId, DateTime createTime, IServiceProvider ServiceProvider) : Lobby(id, createUserId, createTime, ServiceProvider)
+{
+}
+
+public record ChickenDinnerLobbyOptions
+{
+}
